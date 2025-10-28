16ba7343461b835798d634d1b3fd2e36a7007901
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2025-10-28 14:56:14 +0900

    [level 2] Title: 문자열 압축, Time: 2.45 ms, Memory: 78.9 MB -BaekjoonHub

--- Java/프로그래머스/2/60057. 문자열 압축/README.md
+++ Java/프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 142 MB, 시간: 147.82 ms
+메모리: 78.9 MB, 시간: 2.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 31일 13:49:20
+2025년 10월 28일 14:56:09
 
 ### 문제 설명
 
--- Java/프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ Java/프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,42 +1,46 @@
 class Solution {
     public int solution(String s) {
         int answer = s.length();
-        
-        for(int step = 1 ; step < s.length()/2+1 ;step++){
-            String compressed = ""; //압축을 확인 할 때 쓸 문자열
-            String prev = s.substring(0,step); //현재 단위 문자(문자열)
-            int cnt = 1; //반복 횟수 
-            
-            for(int j = step ; j < s.length() ; j += step){ //step만큼씩 증가하는 j
-                String sub = ""; //prev의 비교대상
-                
-                for(int k = j ; k < j + step ; k++){// 처음엔 j부터 step단위만큼만 sub에 문자를 저장
-                    if(k < s.length()) sub += s.charAt(k);
-                }
-                
-                //비교해서 서로 같으면 cnt증가, 다르면 cnt값 확인 후 압축된 문자열에 연결
-                if(prev.equals(sub)) cnt++;
-                else{
-                    compressed += (cnt >= 2) ? cnt+prev : prev;
-                    
-                    //이제 다른걸 알았으니 비교하해당 위치부터 다시 시작
-                    sub = "";
-                    for(int k = j ; k < j+ step ; k++){
-                        if(k < s.length()) sub += s.charAt(k);
+
+        for (int step = 1; step <= s.length() / 2; step++) {
+            int pos = 0; //연산 수행 위치
+            int len = s.length();
+
+            for (; pos + step <= s.length(); ) {
+                String unit = s.substring(pos, pos + step);
+                pos += step;
+
+                int cnt = 0;
+                for (; pos + step <= s.length(); ) {
+                    if (unit.equals(s.substring(pos, pos + step))) {
+                        cnt++;
+                        pos += step;
+                    } else {
+                        break;
                     }
-                    prev = sub;
-                    cnt = 1;
+                }
+
+                if (cnt > 0) {
+                    len -= step * cnt;
+
+                    /*
+                     * 입력은 최대 1000이다.
+                     */
+
+                    if (cnt < 9)
+                        len += 1; //숫자가 1자리 수 이라면 1개만늘리고
+                    else if (cnt < 99)
+                        len += 2; //숫자가 2자리 수 이라면 2개를 늘린다.
+                    else if (cnt < 999)
+                        len += 3; //숫자가 3자리 수 이라면 3개를 늘린다.
+                    else
+                        len += 4;
+
                 }
             }
-            
-            //만약 문자열 s의 길이가 prev로 나누어 떨어지지 않는다면 
-            //문자열 s의 마지막 부분은  "j < s.length() ; j += step"에 의해 만족 못하니깐, 이어붙여줌
-            compressed += (cnt >= 2) ? cnt + prev : prev;
-            
-            //마지막으로 매번 step에 대해 끝날 때 마다 비교해준다.
-            answer = Math.min(compressed.length(), answer); 
+            answer = Math.min(answer, len);
         }
-       
         return answer;
     }
 }
+//aaa
