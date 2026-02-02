cb7a6e9dcbca23d7e5ed43735fa02adc394d0c0f
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-02-02 17:53:46 +0900

    Update 이중 우선순위 큐.java

--- Java/백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ Java/백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,56 +1,52 @@
 import java.util.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
 class Main {
-    public static PriorityQueue minHeap;
-    public static PriorityQueue maxHeap;
-    public static HashMap<Integer, Integer> map;
-    
     public static int tc;
     public static int n;
 
-    public static StringTokenizer st;
     public static StringBuilder sb;
+    public static StringTokenizer st;
     public static BufferedReader br;
+
+    public static PriorityQueue<Integer> minHeap;
+    public static PriorityQueue<Integer> maxHeap;
+    public static HashMap<Integer, Integer> map;
     
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         tc = Integer.parseInt(br.readLine());
-        sb = new StringBuilder();
 
-       
         for(int testCase = 0 ; testCase < tc ; testCase++){
-            minHeap = new PriorityQueue<>();
-            maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-            map = new HashMap<>();
+            n = Integer.parseInt(br.readLine());
 
-            int n = Integer.parseInt(br.readLine());
+            sb = new StringBuilder();
+            minHeap = new PriorityQueue<Integer>();
+            maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+            map = new HashMap<>();
+            
 
             for(int i = 0 ; i < n ; i++){
-                st = new StringTokenizer(br.readLine());
 
+                st = new StringTokenizer(br.readLine());
                 String key = st.nextToken();
-                int value = Integer.parseInt(st.nextToken());
+                int val = Integer.parseInt(st.nextToken());
 
                 if(key.equals("D")){
-                    if(map.isEmpty()) continue;
-
-                    if(value == -1){
+                    if(val == -1){
                         delete(minHeap);
                     }
                     else{
                         delete(maxHeap);
-                
                     }
                 }
-                else{ //key == "I"
-                    map.put(value, map.getOrDefault(value, 0) + 1);
-                    minHeap.offer(value);
-                    maxHeap.offer(value);
+                else{
+                    map.put(val, map.getOrDefault(val, 0) + 1);
+                    minHeap.offer(val);
+                    maxHeap.offer(val);
                 }
             }
-            
+            //반복문이 종료되면, 마지막에 동기화가 안되어있으므로 한번 더 해준다.
             clean(minHeap);
             clean(maxHeap);
             
@@ -60,29 +56,40 @@ class Main {
             else{
                 sb.append(maxHeap.poll()).append(" ");
                 sb.append(minHeap.poll()).append("\n");
-            }
+            }        
+            System.out.print(sb);
+            
         }
-        System.out.println(sb);
+
+        
     }
-
-    public static void delete(PriorityQueue<Integer> q){
+    
+    public static void delete(PriorityQueue<Integer> q) {
+        //1. 일단 맵 동기화시킴
         clean(q);
-
+        
         if(!q.isEmpty()){
             int num = q.poll();
-
             int cnt = map.get(num);
-
-            if(cnt == 1) map.remove(num);
-            else map.put(num, cnt - 1);
+            
+            //하나밖에 안남았으면 삭제시킴
+            if(cnt == 1) 
+                map.remove(num); 
+            //그게 아니라면 하나 감소시킴
+            else 
+                map.put(num, cnt - 1); 
         }
-        
     }
 
     public static void clean(PriorityQueue<Integer> q){
+        /*
+        * 동기화 시키는 로직
+        * 1. 큐가 비어있지 않으면서,
+        * 2. 큐의 맨 앞의 값을 확인하고, 해당 값이 HashMap에 존재하지않는다면?
+        * 3. 삭제한다.
+        */
         while(!q.isEmpty() && !map.containsKey(q.peek())){
-            q.poll(); //비어있지않으면서, 맵에 해당 큐의 키가 없다면?
+            q.poll();
         }
     }
-    
 }
