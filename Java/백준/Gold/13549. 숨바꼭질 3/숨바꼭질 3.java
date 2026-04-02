c414ae8916757370eb530b51472157f11fa97851
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-04-02 12:03:13 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 144 ms, Memory: 17272 KB -BaekjoonHub

--- Java/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ Java/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,28 +1,19 @@
 import java.util.*;
 import java.io.*;
 
-class Node implements Comparable<Node> {
+class Node implements Comparable<Node>{
     private int pos;
     private int time;
+    public Node(int pos, int time) { this.pos = pos; this.time = time;}
 
-    public Node(int pos, int time){
-        this.pos = pos;
-        this.time = time;
-    }
-
-    public int getPos(){
-        return pos;
-    }
-    public int getTime(){
-        return time;
-    }
+    public int getPos(){ return pos; }
+    public int getTime() { return time; }
 
     @Override
     public int compareTo(Node others){
         return this.time - others.time;
     }
 }
-
 
 class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,10 +22,10 @@ class Main {
 
     public static int n, k;
 
-    public static PriorityQueue<Node> q = new PriorityQueue<>();
-    
     public static boolean[] visited = new boolean[100001];
-    
+
+    public static PriorityQueue<Node> pq = new PriorityQueue<>();
+
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
 
@@ -45,36 +36,33 @@ class Main {
             System.out.println(0);
             return;
         }
-        
-        q.add(new Node(n, 0));
-        
-        
-        
-        while(!q.isEmpty()){
-            Node now = q.poll();
+
+        pq.add(new Node(n, 0));
+
+        while(!pq.isEmpty()){
+            Node now = pq.poll();
             int pos = now.getPos();
             int time = now.getTime();
-                        
-            if(visited[pos]) continue;            
+
+            if(visited[pos]) continue;
             visited[pos] = true;
-            
+
             if(pos == k){
                 sb.append(time);
                 break;
             }
-            
+
+
             if(pos - 1 >= 0 && !visited[pos-1]){
-                q.add(new Node(pos - 1, now.getTime() + 1));
+                pq.add(new Node(pos-1, time + 1));
             }
             if(pos * 2 <= 100000 && !visited[pos*2]){
-                q.add(new Node(pos * 2, now.getTime()));
+                pq.add(new Node(pos*2, time));
             }
             if(pos + 1 <= 100000 && !visited[pos+1]){
-                q.add(new Node(pos + 1, now.getTime() + 1));
+                pq.add(new Node(pos+1, time + 1));
             }
-            
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
