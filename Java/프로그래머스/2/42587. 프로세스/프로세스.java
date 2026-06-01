ee5207b8762e667da649a28f0e2fe1a6119e5269
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-06-01 14:00:18 +0900

    [level 2] Title: 프로세스, Time: 3.24 ms, Memory: 64.2 MB -BaekjoonHub

--- Java/프로그래머스/2/42587. 프로세스/프로세스.java
+++ Java/프로그래머스/2/42587. 프로세스/프로세스.java
@@ -11,38 +11,42 @@ class Node {
 }
 
 class Solution {
-    public int solution(int[] priorities, int location) {     
-        int len = priorities.length;
-        
+    public int solution(int[] priorities, int location) {
         Queue<Node> q = new LinkedList<>();
         
-        for(int i = 0 ; i < len ; i++) {
+        /** 세팅 */
+        for(int i = 0 ; i < priorities.length ; i++) {
             q.add(new Node(priorities[i], i));
         }
+        
+        
+        /** 로직 */
         int cnt = 0;
         while(!q.isEmpty()) {
-            
+            // 1. 하나 꺼내기
             Node cur = q.poll();
-            boolean hasHigh = false;
             
-            for(Node n : q) {
-                if(n.val > cur.val) {
-                    hasHigh = true;
+            // 2. 값 비교하기
+            boolean hasHigher = false;
+            for(Node next : q) {
+                if(cur.val < next.val) {
+                    hasHigher = true;
                     break;
                 }
             }
             
-            if(hasHigh) {
+            // 3. 더 높은게 있다면 다시 넣고, 아니라면 실행횟수 추가하기
+            if(hasHigher){
                 q.add(cur);
-            } else{
+            } else {
                 cnt++;
                 
-                if(cur.loc == location) {
-                    return cnt;
-                }
+                // 4. 만약, location과 일치하는 Node라면 return
+                if(location == cur.loc) return cnt;
             }
-            
         }
+        
+        /** 예외처리*/
         return -1;
     }
 }
