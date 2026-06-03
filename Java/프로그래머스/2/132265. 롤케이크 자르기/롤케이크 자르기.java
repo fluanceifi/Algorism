b84038d9616fc81dff259b7c4def291516341d91
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-06-03 17:19:13 +0900

    [level 2] Title: 롤케이크 자르기, Time: 37.24 ms, Memory: 134 MB -BaekjoonHub

--- Java/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ Java/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -4,28 +4,34 @@ class Solution {
     public int solution(int[] topping) {
         int result = 0;
         
-        // 동생 Hash, 형 Hash Set을 생성
-        HashMap<Integer, Integer> brother = new HashMap<>();
-        HashMap<Integer, Integer> chulSoo = new HashMap<>();
+        int[] brother = new int[10001];
+        int cnt = 0;
         
-        // 동생 Hash를 먼저 다 담음
+        HashSet<Integer> chulSoo = new HashSet<>();
+        
         for(int t : topping) {
-            brother.put(t, brother.getOrDefault(t, 0) + 1);
+            if(brother[t] == 0) {
+                cnt++;
+            }
+            
+            brother[t]++;
         }
         
-        // 다시 앞쪽부터 형이 하나씩 담고, 해당하는 값을 동생이 하나씩 뺌, 0이 될 경우 제거
         for(int t : topping) {
-            chulSoo.put(t, chulSoo.getOrDefault(t, 0) + 1);
-            
-            brother.put(t, brother.get(t) - 1);
-            
-            if(brother.get(t) == 0) brother.remove(t);
-            
-            if(brother.size() == chulSoo.size()) result++;
-        }
+            chulSoo.add(t); // Set은 중복 제거
         
-        // 매번 for문 마지막에 개수가 같으면 result ++; 로 값을 증가
+            brother[t]--;
+            
+            if(brother[t] == 0) {
+                cnt--;
+            }
+            
+            if(chulSoo.size() == cnt){
+                result++;
+            }
+        }
         
         return result;
+        
     }
 }
