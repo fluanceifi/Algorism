35765ba956cbbed0581f2f80ef8b8dc86670773e
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-07-15 20:10:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.82 ms, Memory: 57.6 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 59.2 MB, 시간: 3.38 ms
+메모리: 57.6 MB, 시간: 4.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 03일 17:39:26
+2026년 07월 15일 20:10:54
 
 ### 문제 설명
 
--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,9 +1,62 @@
+// import java.util.*;
+
+// class Solution {
+//     public static int[] dy = {-1, 0 , 1, 0}; //북 동 남 서 
+//     public static int[] dx = {0, 1, 0 , -1};
+//     public static int minDist;
+    
+//     public int solution(int[][] maps) {
+//         int n = maps.length;
+//         int m = maps[0].length;
+        
+//         minDist = (int) 1e9;
+
+        
+//         boolean[][] visited = new boolean[n][m];
+//         visited[0][0] = true;
+        
+//         dfs(0, 0, 1, maps, visited);
+        
+//         return minDist == (int) 1e9 ? -1 : minDist;
+//     }
+    
+//     public static void dfs(int cy, int cx, int cnt, int[][] maps, boolean[][] visited) {
+//         int n = maps.length;
+//         int m = maps[0].length;
+        
+//         // 종료 조건 확인
+//         if(cy == n-1 && cx == m-1){
+//             minDist = Math.min(minDist, cnt);
+//             return;
+//         }
+        
+//         // 가지치기
+//         if(cnt >= minDist) return;
+        
+        
+//         // 다음 위치 방문
+//         for(int i = 0 ; i < 4 ; i++){
+//             int ny = cy + dy[i];
+//             int nx = cx + dx[i];
+            
+//             if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+//             // 방문안했으면 dfs
+//             if(maps[ny][nx] == 1 && !visited[ny][nx]) {
+//                 visited[ny][nx] = true;
+//                 dfs(ny, nx, cnt + 1, maps, visited);
+//                 visited[ny][nx] = false;
+//             }
+//         }
+        
+//     }
+// }
 import java.util.*;
+
 class Node {
     int y;
     int x;
     
-    public Node(int y, int x) {
+    public Node(int y, int x){
         this.y = y;
         this.x = x;
     }
@@ -12,31 +65,32 @@ class Node {
 class Solution {
     public static int[] dy = {-1, 0 , 1, 0}; //북 동 남 서 
     public static int[] dx = {0, 1, 0 , -1};
+    public static boolean[][] visited;
     
     public int solution(int[][] maps) {
-        Queue<Node> q = new LinkedList<>();
-        
         int n = maps.length;
         int m = maps[0].length;
         
+        Queue<Node> queue = new LinkedList<>();
+        visited = new boolean[n][m];
         
-        q.add(new Node(0, 0));
+        queue.offer(new Node(0, 0));
+        visited[0][0] = true;
         
-        while(!q.isEmpty()) {
-            Node cur = q.poll();
+        while(!queue.isEmpty()){
+            Node cur = queue.poll();
             
-            if(cur.y == n-1 && cur.x == m-1) break;
-            
-            for(int i = 0 ; i < 4 ; i++) {  
+            for(int i = 0 ; i < 4 ; i++){
                 int ny = cur.y + dy[i];
                 int nx = cur.x + dx[i];
                 
-                if(ny >= n || ny < 0 || nx >= m || nx < 0 ) continue;
-                
+                // 1. 배열 이탈
+                if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                // 2. 예외조건: 벽
                 if(maps[ny][nx] != 1) continue;
                 
-                q.add(new Node(ny, nx));
                 maps[ny][nx] = maps[cur.y][cur.x] + 1;
+                queue.offer(new Node(ny, nx));
             }
         }
         
