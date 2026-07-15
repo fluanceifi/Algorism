4d024a988374bf2825d73157c2f031dbaa0aa459
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-07-16 01:08:10 +0900

    [level 3] Title: 여행경로, Time: 0.97 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    public static int n;
+    public static List<String> list;
+    public static boolean[] visited;
+    public static boolean done;
+    
+    public String[] solution(String[][] tickets) {
+        n = tickets.length;
+        list = new ArrayList<>();
+        visited = new boolean[n];
+        
+        //tickets의 [][1]에 해당하는 값을 사전 순으로 정렬하여 for문으로 처리할 때 복잡한 로직을 필요로하지않도록한다.
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        
+        dfs("ICN", 0, tickets);
+        
+        String[] answer = new String[list.size()];
+        for(int i = 0 ; i < list.size() ; i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+    
+    public static void dfs(String curr, int cnt, String[][] tickets) {
+        // 1. 종료
+        if(cnt == n){
+            done = true;
+            list.add(curr);
+            return;
+        }
+        
+        
+        
+        // 3. 로직
+        for(int i = 0 ; i < n ; i++) {
+            if(tickets[i][0].equals(curr) && !visited[i]) {
+                visited[i] = true;
+                list.add(tickets[i][0]);
+                
+                dfs(tickets[i][1], cnt + 1 , tickets);
+                if(done) return;
+                visited[i] = false;
+                list.remove(list.size() - 1);
+
+            }
+        }
+    }
+}
