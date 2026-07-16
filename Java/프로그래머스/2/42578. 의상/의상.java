d128e2717b01aaa7d69cef8bd9fcb77e06e47ee8
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-07-16 19:36:41 +0900

    [level 2] Title: 의상, Time: 0.25 ms, Memory: 75 MB -BaekjoonHub

--- Java/프로그래머스/2/42578. 의상/의상.java
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -2,18 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        // [0]: 의상이름, [1]: 의상종류
+        /* 
+            1. 각 종류별로 최대 1가지 의상만 착용 가능하다.
+            2. 의상이 겹쳐도 조합만 다르면 됨.
+            3. 의상을 안입어도되는데 일단, 적어도 종류별로 하나는 입어야됨.
+            -> 안입어도되는 경우의 수를 추가로 더한다.
+        */
         Map<String, Integer> map = new HashMap<>();
         
+        // [][0]: 이름 , [][1]: 종류
+        // 중복된 옷이 없다. -> 있다면 별도로 셋 객체 초기화 한 다음 if(contains())로 검증 후 이름을 추가했을 듯
         for(int i = 0 ; i < clothes.length ; i++) {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
-        int n = 1;
         
-        for(Integer m : map.values()) {
-            n *= (m + 1);
+        int result = 1;
+        
+        // get은 특정 Key를 지정해야 되기 때문에 values()를 사용한다.
+        for(int n : map.values()){
+            result *= (n + 1);
         }
-        return n - 1; 
         
+        return result - 1;
     }
 }
