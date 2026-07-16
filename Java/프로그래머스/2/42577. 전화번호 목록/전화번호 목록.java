4c4b6ae98e82c4153d631dfc7bd88290d5ac8ca3
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-07-16 19:05:32 +0900

    [level 2] Title: 전화번호 목록, Time: 181.51 ms, Memory: 234 MB -BaekjoonHub

--- Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,23 +2,24 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {        
-        Map<String, Integer> m = new HashMap<>();
+        Set<String> set = new HashSet<>();
         
-        for(int i = 0 ; i < phone_book.length ; i++) {
-            m.put(phone_book[i], i);
+        // 전화번호부를 다 넣어둔다.
+        for(String s : phone_book) {
+            set.add(s);
         }
         
+        // phone_book[i].substring(0, j)로 나의 일부분이 전화번호부에 존재하는지 확인한다.
+        // 그게 곧 접두어가 되니깐.
         for(int i = 0 ; i < phone_book.length ; i++) {
             for(int j = 1 ; j < phone_book[i].length() ; j++) {
-                String prefix = phone_book[i].substring(0, j);
-                
-                if(m.containsKey(prefix)){
+                if(set.contains(phone_book[i].substring(0, j))) {
                     return false;
                 }
             }
         }
         
         return true;
+        
     }
-    
 }
