cbabd4c5c3d507e5dad7e14b6c8b864ffd76cd80
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-07-19 23:52:47 +0900

    [level 2] Title: 주식가격, Time: 19.62 ms, Memory: 76.2 MB -BaekjoonHub

--- Java/프로그래머스/2/42584. 주식가격/주식가격.java
+++ Java/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,55 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        // 1 2 3 2 3
-        
-        // 방법 1
-        
-        /*
+        // i 번째에서 i + 1, i+ 2, ... prices.length() - 1 까지 i보다 안떨어 졌다면 매순간 answer[i]에 ++ 해준다. (100,000^2 == 10,000,000,000)
         int[] answer = new int[prices.length];
         
-        for(int i = 0 ; i < prices.length - 1 ; i++) {
-            int val = 0;
-            
-            for(int j = i + 1 ; j < prices.length ; j++) {
-                val++;
-                
-                // 가격이 떨어지면 멈춘다.
-                if(prices[i] > prices[j]) {
-                    break;
-                }
-            }
-            
-            answer[i] = val;
-        }
+//         for(int i = 0 ; i < prices.length - 1 ; i++) {
+//             for(int j = i + 1 ; j < prices.length ; j++) {
+//                 if(prices[i] <= prices[j]) answer[i]++;
+//             }
+//         }
         
-        return answer;
-        */
         
-        // 방법 2
-        // Stack에 인덱스 값을 넣거나, 이미 Stack에 인덱스 값이 존재한다면 현재 값보다 큰지 작은지 비교해본다.
-        // 만약 현재 값이 더 작은 경우 스택의 값을 pop 해준다.
-        // > idx = s.pop()  -> answer[idx] = i - idx; == 버틴 시간
-        
+        /* Stack으로 O(n) 접근법 */
         Stack<Integer> s = new Stack<>();
-        int[] answer = new int[prices.length];
         
-        //중간에 값이 떨어져서 결정나는 애들 
-        for(int i = 0 ; i < prices.length ; i++) {
+        for(int i = 0 ; i < prices.length ; i++){ 
             while(!s.isEmpty() && prices[s.peek()] > prices[i]) {
                 int idx = s.pop();
-                answer[idx] = i - idx; 
-            } 
+                answer[idx] = i - idx;
+            }
             s.push(i);
         }
         
-        // 마지막까지 남은 애들
-        while(!s.isEmpty()){
+        /*
+        2 <--- 3(i) - 2 = 1 // 가격이 내려갔을때, 현재 3(i)과 직전 인덱스(2)를 빼면 직전 인덱스가 유지 한 개수가 된다. 
+        1
+        0
+        */
+        
+        while(!s.isEmpty()) {
             int idx = s.pop();
-            answer[idx] = (prices.length - 1) - idx;
+            answer[idx] = prices.length - 1 - idx; // 전체 운영 기간 - 내 위치
         }
         
-        
         return answer;
+        
     }
 }
