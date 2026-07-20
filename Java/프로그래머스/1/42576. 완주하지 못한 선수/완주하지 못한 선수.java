bd05432576b04df98ca7cc79785f02830047bb31
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-07-20 10:44:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.58 ms, Memory: 116 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,33 +2,35 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // HashMap = Map이란 말과 일치하게  Key와 Value를 Mapping하여 저장한다.
-        // HashSet = 객체 그 자체를 저장한다. 
+        // 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주
+    
+        /*
+        마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다. -> n^2불가
+        completion의 길이는 participant의 길이보다 1 작습니다.
+        참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+        참가자 중에는 동명이인이 있을 수 있습니다.
+        */
         
-        // 1. 참여자를 모두 해쉬셋에 넣는다.
-        // 2. completion을 탐색하여 해시셋에 존재하면 삭제한다.
-        // 3. 나머지는 String으로 반환한다.
-        // 문제는 동명이인을 어떻게 처리하냐인데? 
-        // 배열을 정렬시켜서 !equals로 판단하기 vs HashMap으로 Value값을 저장하기 
-        String answer = "";
+        // 해시맵으로 participant를 조회하며 "이름:횟수" 를 저장한다.
+        // completion을 조회하며 "이름"을 조회 후, 횟수를 차감한다.
+        // 차감한 값이 0이되면 삭제한다.
+        // 남은 값을 반환한다.
+        
         Map<String, Integer> map = new HashMap<>();
-        int n = participant.length;
         
-        for(int i = 0 ; i < n ; i++) {
+        for(int i = 0 ; i < participant.length ; i++) {
             map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         
-        for(String s : completion){
-            if(map.containsKey(s))
-                map.put(s, map.get(s) - 1);
+        for(int i = 0 ; i < completion.length ; i++) {
+            if(map.get(completion[i]) == 1) map.remove(completion[i]);
+            else map.put(completion[i], map.get(completion[i]) - 1);
         }
         
-        for(String key : map.keySet()){
-            if(map.get(key) != 0) {
-                answer = key;
-            }
+        for(String s : map.keySet()){
+            return s;
         }
-        return answer;
-        
+        return "";
+    
     }
 }
