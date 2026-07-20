f9ed042c2c594aa4ff85ec6405e3442612e84c8a
FluanceIFI <111571081+fluanceifi@users.noreply.github.com>
2026-07-20 20:43:45 +0900

    [level 1] Title: 모의고사, Time: 3.82 ms, Memory: 89.2 MB -BaekjoonHub

--- Java/프로그래머스/1/42840. 모의고사/모의고사.java
+++ Java/프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,55 +1,54 @@
 import java.util.*;
 
 class Solution {
-    // 1번: [1, 2, 3, 4, 5]
-    // 2번: [2, 1, 2, 3, 2, 4, 2, 5]
-    // 3번: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    public static int[] num1 = {1, 2, 3, 4, 5};
-    public static int[] num2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    public static int[] num3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+//     public static int[] student1 = {1, 2, 3, 4, 5};
+//     public static int[] studnet2 = {2, 1, 2, 3, 2, 4, 2, 5};
+//     public static int[] studnet3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
     
     public int[] solution(int[] answers) {
-        Student s1 = new Student(1, 0);
-        Student s2 = new Student(2, 0);
-        Student s3 = new Student(3, 0);
+        /*
+            10,000 * 10,000 = 100,000,000 -> n^2 가능
+            if (answers[i] == studnets[0][i % students[0].length]) cnt[0]++;
+            if (answers[i] == studnets[1][i % students[1].length]) cnt[1]++;
+            if (answers[i] == studnets[2][i % students[2].length]) cnt[2]++;
+            
+            int[] temp = Arrays.copyOfRange(cnt, 0, 3);
+            Arrays.sort(temp);
+            
+            for(int i = 0 ; i < 3 ; i++) { if(temp[2] == cnt[i]) answer.add(i)}
+            
+            Collections.sort(answer);
+            
+            return answer.stream().mapToInt(Integer::intValue).toArray();
+        */
+        int[][] students = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
         
-        for(int i = 0 ; i < answers.length ; i++){
-            if(answers[i] == num1[i % num1.length]) s1.score++;
-            if(answers[i] == num2[i % num2.length]) s2.score++;
-            if(answers[i] == num3[i % num3.length]) s3.score++;
-        }
-        
-        PriorityQueue<Student> pq = new PriorityQueue<>();
-        pq.offer(s1); pq.offer(s2); pq.offer(s3);
-        
-        Student result = pq.poll();
+        int[] cnt = new int[3];
         
         List<Integer> answer = new ArrayList<>();
         
-        answer.add(result.id);
-        
-        for(Student s : pq) {
-            if(result.score == s.score){
-                answer.add(s.id);
-            }
+        for(int i = 0 ; i < answers.length ; i++) {
+            if (answers[i] == students[0][i % students[0].length]) cnt[0]++;
+            if (answers[i] == students[1][i % students[1].length]) cnt[1]++;
+            if (answers[i] == students[2][i % students[2].length]) cnt[2]++;
+            
         }
         
+        int[] temp = Arrays.copyOfRange(cnt, 0, 3);
+        Arrays.sort(temp);
+            
+        for(int i = 0 ; i < 3 ; i++) { 
+            if(temp[2] == cnt[i]) 
+                answer.add(i+ 1); 
+        }
+            
+        Collections.sort(answer);
+            
         return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-}
-
-class Student implements Comparable<Student> {
-    int id;
-    int score;
-    
-    public Student(int id, int score) {
-        this.id = id;
-        this.score = score;
-    }
-    
-    @Override
-    public int compareTo(Student o) {
-        if(o.score == this.score) return this.id - o.id;
-        return o.score - this.score;
+        
     }
 }
